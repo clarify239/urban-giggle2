844b0ad7575940f62f4550d4e1e6b83973dd6a62
claire <114790844+clarify239@users.noreply.github.com>
2025-02-08 17:57:45 -0500

    added stuffies

--- myFirstGame/src/main/GamePanel.java
+++ myFirstGame/src/main/GamePanel.java
@@ -2,25 +2,103 @@ package main;
 
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
 
 import javax.swing.JPanel;
 
-public class GamePanel extends JPanel{
+import entity.Player;
+
+public class GamePanel extends JPanel implements Runnable{
 
 	//SCREEN SETTINGS
 	final int originalTileSize = 16;
 	final int scale = 3;
 	
-	final int tileSize = originalTileSize * scale; //48x48 tile
+	public final int tileSize = originalTileSize * scale; //48x48 tile
 	final int maxScreenCol = 16;
 	final int maxScreenRow = 12;
 	final int screenWidth = tileSize * maxScreenCol;
 	final int screenHeight = tileSize * maxScreenRow;
+	
+	//FPS
+	int FPS = 60;
+	
+	KeyInput keyI = new KeyInput();
+	Thread gameThread;
+	Player player = new Player(this, keyI);
+	
 			
+	//set player default pos
+	int playerX = 100;
+	int playerY = 100;
+	int playerSpeed = 4;
+	
 	public GamePanel() {
 		this.setPreferredSize(new Dimension(screenWidth, screenHeight));
-		this.setBackground(Color.black);
+		this.setBackground(Color.white);
 		this.setDoubleBuffered(true);
+		this.addKeyListener(keyI);
+		this.setFocusable(true);
 	}
+
+	public void startGameThread() {
+		gameThread = new Thread(this);
+		gameThread.start();
+	}
+
+	@Override
+	public void run() {
+		
+		double drawInterval = 1000000000/FPS; //0.01666 seconds
+		double nextDrawTime = System.nanoTime() + drawInterval;
+		
+		while(gameThread!=null) {
+			
+//			long currentTime = System.nanoTime();
+//			System.out.println("current time: " + currentTime);
+			
+			//update information  
+			update();
+			
+			//draw screen w updated 
+			repaint();
+			
+			try {
+				double remainingTime = nextDrawTime - System.nanoTime();
+				remainingTime = remainingTime/1000000;
+				
+				Thread.sleep((long) remainingTime);
+				
+				if(remainingTime<0) {
+					remainingTime = 0;
+				}
+				
+				nextDrawTime +=drawInterval;
+				
+			} catch(InterruptedException e) {
+				e.printStackTrace();
+			}
+			
+		}
+	}
+		
+		
+	public void update() {
+		player.update();
+		
+	}
+	
+	public void paintComponent(Graphics g) {
+		
+		super.paintComponent(g);
+		
+		Graphics2D g2 = (Graphics2D)g;
+		
+		player.draw(g2);
+		
+		g2.dispose();
+	}
+	
 
 }

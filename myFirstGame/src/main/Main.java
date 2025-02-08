46f67756d1f1d5bb350f5e8edc5b8e72bec3fccc
claire <114790844+clarify239@users.noreply.github.com>
2025-02-08 16:50:24 -0500

    initial commit frfr

---
+++ myFirstGame/src/main/Main.java
@@ -0,0 +1,24 @@
+package main;
+
+import javax.swing.JFrame;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		JFrame window = new JFrame();
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		window.setResizable(false);
+		window.setTitle("My First Java 2D Game");
+		
+		GamePanel gamePanel = new GamePanel();
+		window.add(gamePanel);
+		System.out.println("test");
+		//test
+		window.pack();
+		
+		window.setLocationRelativeTo(null);
+		window.setVisible(true);
+	}
+
+}
